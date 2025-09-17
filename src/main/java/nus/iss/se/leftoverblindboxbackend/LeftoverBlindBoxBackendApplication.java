61d3743a9673cbd10e9670c53d97c3ff44edaa35
fuzhengpan <panfuzheng16@foxmail.com>
2025-09-17 10:22:34 +0800

    create

---
+++ src/main/java/nus/iss/se/leftoverblindboxbackend/LeftoverBlindBoxBackendApplication.java
@@ -0,0 +1,13 @@
+package nus.iss.se.leftoverblindboxbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeftoverBlindBoxBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeftoverBlindBoxBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nus/iss/se/leftoverblindboxbackend/LeftoverBlindBoxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nus.iss.se.leftoverblindboxbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeftoverBlindBoxBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
