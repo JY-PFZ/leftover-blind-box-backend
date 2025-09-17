ffedf7fd6594275b97fe42f22fb3f4d8ba6dc125
fuzhengpan <panfuzheng16@foxmail.com>
2025-09-18 00:51:18 +0800

    1.user login 2.register 3.Integrate Spring Security and JWT

--- src/main/java/nus/iss/se/LeftoverBlindBoxBackendApplication.java
+++ src/main/java/nus/iss/se/LeftoverBlindBoxBackendApplication.java
@@ -1,9 +1,11 @@
-package nus.iss.se.leftoverblindboxbackend;
+package nus.iss.se;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("nus.iss.se.magicbag.mapper")
 public class LeftoverBlindBoxBackendApplication {
 
     public static void main(String[] args) {
